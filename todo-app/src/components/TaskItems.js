4a4c0f6224b2c436d438c4d76f12b2145a046849
Truong Pham Nhat <pntruong1@tma.com.vn>
2019-09-10 13:46:40 +0700

    fix problem "chicken" :v

--- todo-app/src/components/TaskItems.js
+++ todo-app/src/components/TaskItems.js
@@ -21,28 +21,18 @@ const TaskItems = props => {
      * Show the list items base on status isComplete
      */
 
-    const itemComplete = props.tasks.filter((task, index) => {
-        if (task.isComplete) {
-            return task
-        }
-    }).map((task,index) => {
+    const itemComplete = props.tasks.filter((task, index) => task.isComplete).map((task,index) => {
         return <li className="list-group-item" key={task.id}>{task.value}
                 <button className="float-right fas fa-trash btn btn-danger ml-2" onClick={() => props.onDelete(task.id)}></button>
                 <button className="float-right fas fa-undo btn btn-info" onClick={() => props.onUndo(task.id)}></button></li>
     })
-    const itemTodo = props.tasks.filter((task, index) => {
-        if (!task.isComplete) {
-            return task
-        }
-    }).map((task,index) => {
+    const itemTodo = props.tasks.filter((task, index) => !task.isComplete).map((task,index) => {
         return <li className="list-group-item" key={task.id}>{task.value}
                 <button className="float-right fas fa-check btn btn-danger ml-2" onClick={() => props.onCheck(task.id)}></button>
                 <button className="float-right fas fa-pencil-alt btn btn-warning btn-edit"
                     data-toggle="modal" data-target="#editForm" onClick={() => editItem(task)}></button>
             </li>
     })
-    console.log("itemTdo", itemTodo);
-    console.log("itemCompelete", itemComplete);
     const item = props.isAll ? itemTodo : itemComplete
     return (
         <div>
